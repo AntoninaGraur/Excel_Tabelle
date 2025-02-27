e5c92b3819e68fcdafd657c9a91c45ff6cdaebee
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2025-02-27 15:46:31 +0100

    changing for display csv file

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ class Programm
     {
         System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-        string excelFilePath = "C:/UsersA.Graur/Documents/BKU/SWD/Bestellungen.xlsx";
-        string csvFilePath = "C:/UsersA.Graur/Documents/BKU/SWD/BestellungenCSV.xlsx"; 
+        string excelFilePath = "C:/Users/A.Graur/Documents/BKU/SWD/Bestellungen.xlsx";
+        string csvFilePath = "C:/Users/A.Graur/Documents/BKU/SWD/BestellungenCSV.csv"; 
 
         using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
         {
@@ -23,15 +23,14 @@ class Programm
                 var result = reader.AsDataSet();
 
                 DataTable table = result.Tables[0];
+
+
                 using (var writer = new StreamWriter(csvFilePath))
                 {
                     for (int i = 0; i < table.Columns.Count; i++)
                     {
                         writer.Write(table.Columns[i].ColumnName);
-                        if( i < table.Columns.Count -1 )
-                        {
-                            writer.Write(",");
-                        }
+                        if( i < table.Columns.Count -1) writer.Write(",");
                         writer.WriteLine();
 
                     }
@@ -46,9 +45,19 @@ class Programm
                         writer.WriteLine();
                     }
                 }
+                //Datei auf dem Bildschirm ausgeben
+                Console.WriteLine("Inhalt der Excel-Datei: ");
+                foreach (DataRow  row in table.Rows)
+                {
+                    for (int i=0; i< table.Columns.Count; i++)
+                    {
+                        Console.Write(row[i].ToString() + " ; ");
+                    }
+                    Console.WriteLine(".........");
+                } Console.WriteLine("In CSV umgewandelt.");
+
 
             }
         }
-        Console.WriteLine("In CSV umgewandelt.");
     }
 }
