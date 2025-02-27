ebe3ee155a1690f86261ed409f3841b53722d34d
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2025-02-27 09:26:46 +0100

    add the changes in code

--- Program.cs
+++ Program.cs
@@ -1,32 +1,18 @@
 ﻿
-//using IronXL;
-//using static System.Net.Mime.MediaTypeNames;
-/*using System;
-using System.Data;
-using System.Data.OleDb;
-using System.Windows.Forms;
-using System.Windows.Forms;*/
 
-
-//using Microsoft.Office.Interop.Excel;
-//using Application = Microsoft.Office.Interop.Excel.Application;
-
-
-/*WorkBook workbook = WorkBook.Load("C:/Users/A.Graur/Documents/BKU/SWD/11_LS 5.1_Bestellungen.xlsx");
-
-//WorkSheet worksheet = workbook.WorkSheets["Sheet1"];
-
-WorkSheet worksheet = workbook.WorkSheets[0];
-
-
-Console.WriteLine(worksheet);
-*/
-
-
-//Application excel = new Application();
-//excel.Visible = true;
 
 using System;
 using System.Data;
 using System.IO;
-//using ExcelDataReader;
+using ExcelDataReader;
+
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+        string excelFilePath
+    }
+}
