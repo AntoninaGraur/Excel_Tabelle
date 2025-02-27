a90b49e61b4ec52cbe8352c3d437c336164c3aeb
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2025-02-27 09:37:18 +0100

    Start conwert Excel File in to csv

--- Program.cs
+++ Program.cs
@@ -13,6 +13,17 @@ class Programm
     {
         System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-        string excelFilePath
+        string excelFilePath = "C:/UsersA.Graur/Documents/BKU/SWD/Bestellungen.xlsx";
+        string csvFilePath = "C:/UsersA.Graur/Documents/BKU/SWD/BestellungenCSV.xlsx"; 
+
+        using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
+        {
+            using ( var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                var result = reader.AsDataSet();
+
+                DataTable table = result.Table
+            }
+        }
     }
 }
